f57886505380890c747669ac1a2eb6fac7def65d
Dani Villalba <danivillalba_007@hotmail.com>
2023-01-30 09:13:59 -0300

    asdf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import PersonaCard from "./components/PersonCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Poniendo todo junto
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PersonaCard
+          firstName="Jane"
+          lastName="Doe"
+          age={45}
+          hairColor="Black"
+        ></PersonaCard>
+        <PersonaCard
+          firstName="John"
+          lastName="Smith"
+          age={88}
+          hairColor="Brown"
+        ></PersonaCard>
+      </div>
+    );
+  }
 }
 
 export default App;
