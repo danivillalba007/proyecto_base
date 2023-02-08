d629f9365a28934628388dd7991627041567fc1b
Dani Villalba <danivillalba_007@hotmail.com>
2023-02-07 23:38:06 -0300

    asdf

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import "./App.css";
 import { useState } from "react";
 
 const App = () => {
-  const [actividad, setActividad] = useState("");
+  const [actividad, setActividad] = useState([]);
 
-  // setActividad();
+  const guardarInputs = (e) => {
+    e.preventDefault();
+
+    setActividad([...actividad, e.target[0].value]);
+  };
+
+  // const nuevaActividad = () => {
+  //   actividad.map(setActividad, nuevaActividad);
+  // };
 
   return (
     <div>
-      <div>
-        <input type="text" />
-        <button name="add">add</button>
-        {/* <p>{actividad}</p> */}
-      </div>
+      <form onSubmit={guardarInputs}>
+        <input type="text" name="input" />
+        <button name="addButton">add</button>
+      </form>
+      <h2>Actividades</h2>
+      <p>
+        {actividad.map((actividad) => {
+          return (
+            <div>
+              <p>
+                {actividad}
+                <button> Delete</button>
+              </p>
+            </div>
+          );
+        })}
+      </p>
     </div>
   );
 };
