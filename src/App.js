ea905f5e7869b5dbf1e236ee0f39e112081edfea
Dani Villalba <danivillalba_007@hotmail.com>
2023-02-09 22:25:48 -0300

    asdf

--- src/App.js
+++ src/App.js
@@ -3,26 +3,25 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 const App = () => {
-  const [pokeData, setPokeData] = useState(null);
+  const [pokeData, setPokeData] = useState([]);
 
-  useEffect(() => {
+  const mostrarPokemones = () => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=100")
       .then((respuesta) => {
-        console.log(respuesta, "respuesta exitosa");
         setPokeData(respuesta.data.results);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
 
   return (
     <div>
-      {" "}
+      <button onClick={mostrarPokemones}>Fetch Pokemon</button>
       {pokeData.map((pokemon, indicePokemon) => (
-        <li>{pokemon.name}</li>
-      ))}{" "}
+        <li key={indicePokemon}>{pokemon.name}</li>
+      ))}
     </div>
   );
 };
